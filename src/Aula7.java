5d1fd840565c06e78d09c238515d5bcbcc17bd0b
Layo Mendes <165064021+LayoMendes@users.noreply.github.com>
2025-09-29 18:21:38 +0000

    Aula07

---
+++ src/Aula7.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args) {
+       s
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    })
+}
